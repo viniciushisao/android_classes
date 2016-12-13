884e4e4d49a2b0133159034217fc18ab4fcd0f56
Vinicius Suzuki <vinicius@hisao.com.br>
2016-12-13 18:10:43 -0200

    Fix some shit

--- Service/app/src/main/java/br/com/hisao/service/UsingIntentService.java
+++ Service/app/src/main/java/br/com/hisao/service/UsingIntentService.java
@@ -3,6 +3,7 @@ package br.com.hisao.service;
 import android.app.IntentService;
 import android.content.Intent;
 import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by vinicius on 9/18/16.
@@ -15,6 +16,7 @@ public class UsingIntentService extends IntentService {
      */
     public UsingIntentService() {
         super("UsingIntentService");
+        Log.d("Vinicius", "IntentService started");
     }
 
     /**
@@ -24,17 +26,22 @@ public class UsingIntentService extends IntentService {
      */
     @Override
     protected void onHandleIntent(Intent intent) {
+
         // Normally we would do some work here, like download a file.
         // For our sample, we just sleep for 5 seconds.
-        long endTime = System.currentTimeMillis() + 5*1000;
-        while (System.currentTimeMillis() < endTime) {
+
+        int ammountSeconds = 15;
+        int contSeconts = 0;
+
+        while (ammountSeconds > contSeconts) {
             synchronized (this) {
                 try {
-                    Log.d("UsingIntentService", "Passing here" );
-                    wait(endTime - System.currentTimeMillis());
+                    Log.d("Vinicius", "running IntentService: " + contSeconts++);
+                    wait(1000);
                 } catch (Exception e) {
                 }
             }
         }
+        Log.d("Vinicius", "IntentService finished");
     }
 }
--- Service/app/src/main/java/br/com/hisao/service/UsingService.java
+++ Service/app/src/main/java/br/com/hisao/service/UsingService.java
@@ -15,43 +15,34 @@ public class UsingService extends Service {
     private Looper mServiceLooper;
     private ServiceHandler mServiceHandler;
 
-    // Handler that receives messages from the thread
     private final class ServiceHandler extends Handler {
         public ServiceHandler(Looper looper) {
             super(looper);
         }
         @Override
         public void handleMessage(Message msg) {
-            // Normally we would do some work here, like download a file.
-            // For our sample, we just sleep for 5 seconds.
-            int count = 0;
-            long endTime = System.currentTimeMillis() + 5*1000;
-            while (System.currentTimeMillis() < endTime) {
+            int ammountSeconds = 15;
+            int contSeconts = 0;
+
+            while (ammountSeconds > contSeconts) {
                 synchronized (this) {
                     try {
-                        Log.d("UsingService", "Passing here" );
-                        wait(endTime - System.currentTimeMillis());
+                        Log.d("Vinicius", "running Service : " + contSeconts++);
+                        wait(1000);
                     } catch (Exception e) {
                     }
                 }
             }
-            // Stop the service using the startId, so that we don't stop
-            // the service in the middle of handling another job
             stopSelf(msg.arg1);
         }
     }
 
     @Override
     public void onCreate() {
-        // Start up the thread running the service.  Note that we create a
-        // separate thread because the service normally runs in the process's
-        // main thread, which we don't want to block.  We also make it
-        // background priority so CPU-intensive work will not disrupt our UI.
         HandlerThread thread = new HandlerThread("ServiceStartArguments",
                 Process.THREAD_PRIORITY_BACKGROUND);
         thread.start();
 
-        // Get the HandlerThread's Looper and use it for our Handler
         mServiceLooper = thread.getLooper();
         mServiceHandler = new ServiceHandler(mServiceLooper);
     }
@@ -59,20 +50,14 @@ public class UsingService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Toast.makeText(this, "service starting", Toast.LENGTH_SHORT).show();
-
-        // For each start request, send a message to start a job and deliver the
-        // start ID so we know which request we're stopping when we finish the job
         Message msg = mServiceHandler.obtainMessage();
         msg.arg1 = startId;
         mServiceHandler.sendMessage(msg);
-
-        // If we get killed, after returning from here, restart
         return START_STICKY;
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        // We don't provide binding, so return null
         return null;
     }
 
