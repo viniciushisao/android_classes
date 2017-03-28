64a38ed1c4aabda5e2efbb4b29ae2e9660ef4c53
Vinicius Suzuki <vinicius@Viniciuss-MacBook-Pro.local>
2017-03-27 22:29:31 -0300

    Fix minor mistake in this project

--- MySQLiteSample/app/src/main/java/br/com/hisao/mysqlitesample/MainActivity.java
+++ MySQLiteSample/app/src/main/java/br/com/hisao/mysqlitesample/MainActivity.java
@@ -1,7 +1,7 @@
 package br.com.hisao.mysqlitesample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
